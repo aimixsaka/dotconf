7d193cf6679cfafd08848b90f11dc8d948836181
aimixsaka <aimixsaka@gmail.com>
2024-10-17 21:48:01 +0800

    nix: add fontconfig
    Former-commit-id: 59edd04acea4f49ec0c6581164148c23eca24b81

--- nix-conf/users/default.nix
+++ nix-conf/users/default.nix
@@ -11,8 +11,8 @@ _:
       system = "x86_64-linux";
       stateVersion = "24.05";
       modules = [
+        ../shared/nixos/wm
         ../shared/nixos/wm/river
-        ../shared/nixos/wm/alacritty.nix
         ./aimi-dell
       ];
     };
