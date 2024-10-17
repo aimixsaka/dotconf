871e900bdb817ccf9dd9bde05621a514455d33bb
aimixsaka <aimixsaka@gmail.com>
2024-10-17 20:10:42 +0800

    nix: add flake substituters

--- nix-conf/flake.nix
+++ nix-conf/flake.nix
@@ -54,4 +54,29 @@
     home.inputs.nixpkgs.follows = "nixpkgs";
     emacs-overlays.inputs.nixpkgs.follows = "nixpkgs";
   };
+
+  # the nixConfig affects the flake itself,
+  # not the system configuration
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
+
+    # nix community's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
 }
