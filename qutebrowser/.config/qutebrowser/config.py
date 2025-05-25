2aa2bbdf8b33a9af59a18a80722d518cc789d038
aimixsaka <aimixsaka@gmail.com>
2025-05-25 21:37:52 +0800

    Revert "qutebrowser: add basic emacs simulation"
    This reverts commit 83bdfb2ffc359794fd7dfc7854cb2114ecc00f2e.

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,6 +2,7 @@ config.load_autoconfig(False)
 
 c.content.pdfjs = True
 c.zoom.default = "130%"
+c.editor.command = ["neovide", "{file}"]
 c.auto_save.session = True
 c.spellcheck.languages = ["en-US"]
 c.tabs.position = "left"
@@ -15,8 +16,6 @@ c.url.searchengines = {
 c.fonts.web.size.default = 18
 # c.fonts.web.size.default_fixed = 20
 
-## Reference: https://git.sr.ht/~willvaughn/dots/tree/main/item/.config/qutebrowser/qutemacs.py
-c.editor.command = ["emacsclient", "-c", "{file}"]
 # mpv
 config.bind(",m", "spawn umpv {url}")
 config.bind(",M", "hint links spawn umpv {hint-url}")
@@ -26,56 +25,10 @@ config.bind("<F2>", "config-edit")
 config.bind("<Ctrl-E>", "config-cycle tabs.show always never")
 config.bind("=", "zoom-in")
 
-## Move related
 config.unbind("d")
 config.unbind("u")
 config.unbind("U")
-config.unbind("<Ctrl-u>")
-config.unbind("<Ctrl-d>")
-# config.unbind("gg")
-# config.unbind("G")
 
-config.bind("<Ctrl-v>", "scroll-page 0 0.5")
-config.bind("<Alt-v>", "scroll-page 0 -0.5")
+config.bind("u", "scroll-page 0 -0.5")
+config.bind("d", "scroll-page 0 +0.5")
 config.bind("U", "undo")
-config.bind("[b", "scroll-to-perc 0")
-config.bind("]b", "scroll-to-perc")
-
-## Command
-config.unbind(":")
-config.unbind("<Escape>")
-config.bind("<Alt-x>", "cmd-set-text :")
-config.bind("<Ctrl-g>", "clear-keychain ;; search ;; fullscreen --leave")
-config.bind("<Ctrl-g>", "mode-leave", mode="insert")
-config.bind("<Ctrl-;>", "mode-leave", mode="insert")
-config.bind("<Ctrl-g>", "mode-leave", mode="command")
-config.bind("<Ctrl-g>", "mode-leave", mode="prompt")
-config.bind("<Ctrl-g>", "mode-leave", mode="register")
-config.bind("<Ctrl-g>", "mode-leave", mode="yesno")
-config.bind("<Ctrl-g>", "mode-leave", mode="hint")
-config.bind("<Ctrl-g>", "mode-leave", mode="caret")
-
-## Caret(visual)
-config.unbind("v")
-config.unbind("V")
-config.unbind("<Space>", mode="caret")
-config.bind("<Ctrl-Space>", "mode-enter caret")
-config.bind("<Ctrl-Space>", "selection-toggle", mode="caret")
-
-
-## Macro
-config.unbind("q")
-config.unbind("@")
-config.bind("<F3>", "macro-record")
-config.bind("<F4>", "macro-run")
-config.bind("<Ctrl-x>(", "macro-record")
-config.bind("<Ctrl-x>)", "macro-record")
-config.bind("<Ctrl-x>e", "macro-run")
-
-## Search
-config.unbind("/")
-config.unbind("?")
-config.bind("<Ctrl-s>", "cmd-set-text /")
-config.bind("<Ctrl-r>", "cmd-set-text ?")
-config.bind("<Ctrl-s>", "search-next", mode="command")
-config.bind("<Ctrl-r>", "search-prev", mode="command")
