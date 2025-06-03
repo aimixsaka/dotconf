92d9442ae4cf7c642a2dbe9f615f3464273d491a
aimixsaka <aimixsaka@gmail.com>
2025-06-03 12:45:57 +0800

    emacs: set default font and size

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -264,6 +264,6 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 150 :width normal :foundry "IBM " :family "IBM Plex Mono")))))
 
 (setq gc-cons-threshold (or bedrock--initial-gc-threshold 800000))
