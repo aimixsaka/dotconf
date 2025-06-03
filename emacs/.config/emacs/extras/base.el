8504961ff1f79fccf567bf30499e8062c187490c
aimixsaka <aimixsaka@gmail.com>
2025-06-03 14:32:12 +0800

    emacs: add project/dired level occur package

--- emacs/.config/emacs/extras/base.el
+++ emacs/.config/emacs/extras/base.el
@@ -6,3 +6,8 @@
   :config
   (eat-eshell-mode)                     ; use Eat to handle term codes in program output
   (eat-eshell-visual-command-mode))     ; commands like less will be handled by Eat
+
+
+;; multi-occur on project or dired
+(use-package noccur
+  :straight (noccur :type git :host github :repo "NicolasPetton/noccur.el"))
