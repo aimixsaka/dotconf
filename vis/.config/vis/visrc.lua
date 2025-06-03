39e8fbd3f98a698ad4324e63f60058c6ce1e9d99
aimixsaka <aimixsaka@gmail.com>
2025-06-04 00:42:31 +0800

    vis: add tabwidth and expandtab for janet and lua

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -18,17 +18,21 @@ end
 
 -- Custom Options --
 vis.events.subscribe(vis.events.INIT, function()
-	vis:command('set shell /bin/bash')
-	--vis:command('map normal <Space><Space> :w *')
+  vis:command('set shell /bin/bash')
+  --vis:command('map normal <Space><Space> :w *')
 end)
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
-	-- Your per window configuration options e.g.
-	vis:command('set number')
-	vis:command('set relativenumber')
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+  -- Your per window configuration options e.g.
+  vis:command('set number')
+  vis:command('set relativenumber')
+  if vis.win.file.name:match('[^/]*.janet') or vis.win.file.name:match('[^/]*.lua') then
+    vis:command('set tabwidth 2')
+    vis:command('set expandtab')
+  end
 end)
 
 -- Custom Commands --
 vis:command_register('xa', function(argv, force, win, selection, range)
-	vis:command('X/.*/wq')
+  vis:command('X/.*/wq')
 end, 'Save and quit all buffers.')
